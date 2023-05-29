b15bcf01ce4dbd42b1d6d06170cef67160497c6e
Ijovany Cecílio <132367847+ijovanycecilio@users.noreply.github.com>
2023-05-28 22:33:26 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Atualizando o servidor..."
+echo "Updating the server..."
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
 
-echo "Baixando e copiando os arquivos da aplicação..."
+echo "Downloading and copying the application files..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
